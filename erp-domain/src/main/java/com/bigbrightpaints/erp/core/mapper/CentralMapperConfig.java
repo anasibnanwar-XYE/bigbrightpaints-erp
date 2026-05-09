42a0d86229b2bbc4f0a28d1e8a80ce95395c757c
Anas <anas@bun-2.local>
2026-05-09 21:26:29 +0530

    remove unused mapper config abstraction

--- erp-domain/src/main/java/com/bigbrightpaints/erp/core/mapper/CentralMapperConfig.java
+++
@@ -1,7 +0,0 @@
-package com.bigbrightpaints.erp.core.mapper;
-
-import org.mapstruct.MapperConfig;
-import org.mapstruct.ReportingPolicy;
-
-@MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface CentralMapperConfig {}
